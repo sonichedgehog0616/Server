51aff9c1b726d76e52da5eab2c9cb37ee33a54bb
mokoti <mokoti@hori-tomokinoMacBook-Pro.local>
2021-11-19 21:36:01 +0900

    nodejsインストール

---
+++ nodejs/app/server.js
@@ -0,0 +1,24 @@
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+
+var cache = {};
+
+function send40(response){
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents){
+    response.writeHead(
+        200,
+        {
+            "Content-type": mime.lookup(path.basename(filePath))
+        }
+    );
+    response.end(fileContents);
+}
+
